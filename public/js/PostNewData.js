1163c302cdfd8f017597767a439123b700f6b183
Lucas N <98057712+Luccxx@users.noreply.github.com>
2023-08-28 19:34:26 -0300

    Adicionando função de Dados Não Salvos

--- public/js/PostNewData.js
+++ public/js/PostNewData.js
@@ -1,12 +1,27 @@
-/*window.addEventListener("beforeunload", function (event) {
-    
-    event.preventDefault();
+let VarDateNotSave = false;
 
-    const mensagem = "Você tem dados não salvos. Tem certeza de que deseja sair desta página?";
-    event.returnValue = mensagem; 
-    return mensagem;
-});
-*/
+function DateSave(){
+    VarDateNotSave = true;
+}
+
+function DateNotSave(){
+    VarDateNotSave = false;
+}
+
+function DateNotSave(VarDateNotSave){
+    if (VarDateNotSave === true) {
+        window.addEventListener("beforeunload", function (event) {
+            event.preventDefault();
+        
+            const mensagem = "Você tem dados não salvos. Tem certeza de que deseja sair desta página?";
+            event.returnValue = mensagem; 
+            return mensagem;
+        });
+    }
+    
+}
+
+
 function FullDate(){
     const data = new Date();
     const day = data.getUTCDate();
@@ -34,18 +49,10 @@ document.getElementById("paymentForm").addEventListener("submit", function (even
         "data_pagamento": FullDate()//document.getElementById('data_pagamento').value
     };
 
-    const test = document.getElementById('situacao').checked;
-    console.log(test);
-
-    console.log(novoPagamento);
-
-    // Adicione o objeto novoPagamento ao array
     dataToSubmit.push(novoPagamento);
 
-    // Crie uma tabela HTML
     const tbody = document.getElementById("tbody-container");
 
-    // Crie uma nova linha (<tr>) para os dados do objeto
     const dataRow = document.createElement("tr");
     for (const key in novoPagamento) {
         const dataCell = document.createElement("td");
@@ -53,37 +60,39 @@ document.getElementById("paymentForm").addEventListener("submit", function (even
         dataRow.appendChild(dataCell);
     }
 
-    // Adicione a nova linha ao tbody
     tbody.appendChild(dataRow);
 
-    //this.reset();
+    this.reset();
+    DateSave();
+    DateNotSave(VarDateNotSave);
 });
 
 document.getElementById("enviarDados").addEventListener("click", function (event) {
     event.preventDefault();
 
     console.log(dataToSubmit);
+    
 
-    // Verifique se há dados para enviar
     if (dataToSubmit.length > 0) {
-        // Envie os dados para o servidor (API PHP, por exemplo)
+        
         fetch("http://localhost/api.php", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(dataToSubmit),// Converte o array de dados para JSON
+            body: JSON.stringify(dataToSubmit),
         })
         .then((response) => {
             if (!response.ok) {
                 throw new Error("Erro ao enviar os dados");
             }
-            window.location.href = "../../index.html";
+            //window.location.href = "../../index.html";
             console.log(response);
+            DateNotSave();
             return response.json();
         })    
         .then((data) => {
-            console.log(data); // A resposta da API após a inserção
+            console.log(data);
         })
         .catch((error) => {
             console.error("Erro:", error);
